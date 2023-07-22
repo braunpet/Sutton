9b9e60075515d1b80895c148c9c2137d2ac6a97b
Tscherner, Philipp <philipp.tscherner@study.thws.de>
2023-07-22 16:10:25 +0200

    added more javadoc

--- src/main/java/de/fhws/fiw/fds/sutton/server/database/hibernate/operations/model/AbstractReadAllOperation.java
+++ src/main/java/de/fhws/fiw/fds/sutton/server/database/hibernate/operations/model/AbstractReadAllOperation.java
@@ -10,16 +10,40 @@ import jakarta.persistence.criteria.*;
 
 import java.util.List;
 
+/**
+ * This abstract class represents an operation to read all entities of type T from the database
+ * based on the provided search parameters. It extends the AbstractDatabaseOperationWithSearchParameter class
+ * to perform the database read operation.
+ *
+ * @param <T> The type of the entity extending AbstractDBModel to be read from the database.
+ */
 public abstract class AbstractReadAllOperation<T extends AbstractDBModel>
         extends AbstractDatabaseOperationWithSearchParameter<T> {
 
+    /**
+     * The class of the entity to be read.
+     */
     private final Class<T> clazz;
 
+    /**
+     * Constructs a new AbstractReadAllOperation with the provided EntityManagerFactory,
+     * class of the entity to be read, and search parameters.
+     *
+     * @param emf             The EntityManagerFactory used for database access.
+     * @param clazz           The class of the entity to be read.
+     * @param searchParameter The search parameters used for filtering and ordering the result set.
+     */
     public AbstractReadAllOperation(EntityManagerFactory emf, Class<T> clazz, SearchParameter searchParameter) {
         super(emf, searchParameter);
         this.clazz = clazz;
     }
 
+    /**
+     * Runs the database operation to read all entities of type T from the database
+     * based on the provided search parameters.
+     *
+     * @return A CollectionModelHibernateResult containing the list of entities read from the database.
+     */
     @Override
     public CollectionModelHibernateResult<T> run() {
         var returnValue = new CollectionModelHibernateResult<>(readResult());
@@ -27,11 +51,15 @@ public abstract class AbstractReadAllOperation<T extends AbstractDBModel>
         return returnValue;
     }
 
+    /**
+     * Reads the result from the database based on the search parameters.
+     *
+     * @return A list of entities of type T read from the database.
+     */
     private List<T> readResult() {
         final CriteriaBuilder cb = em.getCriteriaBuilder();
         final CriteriaQuery<T> cq = cb.createQuery(this.clazz);
         final Root<T> rootEntry = cq.from(this.clazz);
-
 
         final CriteriaQuery<T> all = cq.select(rootEntry)
                 .where(getPredicates(cb, rootEntry))
@@ -45,6 +73,11 @@ public abstract class AbstractReadAllOperation<T extends AbstractDBModel>
                 .getResultList();
     }
 
+    /**
+     * Calculates the total number of entities of type T that match the search parameters.
+     *
+     * @return The total number of entities of type T that match the search parameters.
+     */
     private int getTotalNumberOfResults() {
         final CriteriaBuilder cb = em.getCriteriaBuilder();
         final CriteriaQuery<Long> cq = cb.createQuery(Long.class);
@@ -56,5 +89,5 @@ public abstract class AbstractReadAllOperation<T extends AbstractDBModel>
                 .setHint("org.hibernate.cacheable", true)
                 .getSingleResult().intValue();
     }
-
 }
+
