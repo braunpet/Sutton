616130911473635c16641f62d11bf6d4d6aedea6
Tscherner, Philipp <philipp.tscherner@student.fhws.de>
2023-07-28 14:42:17 +0200

    added more JavaDoc

--- src/main/java/de/fhws/fiw/fds/sutton/server/api/rateLimiting/ApiKeyGenerator.java
+++ src/main/java/de/fhws/fiw/fds/sutton/server/api/rateLimiting/ApiKeyGenerator.java
@@ -12,6 +12,12 @@ import java.util.Collection;
 
 public class ApiKeyGenerator {
 
+    /**
+     * Generates a random API key using a SecureRandom instance with SHA1PRNG algorithm.
+     *
+     * @return A randomly generated API key as a Base64-encoded string.
+     * @throws RuntimeException If the SHA1PRNG algorithm is not available.
+     */
     private static String getRandomKey() {
         SecureRandom sr;
         try {
@@ -24,11 +30,18 @@ public class ApiKeyGenerator {
         return Base64.getEncoder().encodeToString(random);
     }
 
+    /**
+     * Generates a unique API key with the specified reset rate and request limit.
+     *
+     * @param resetRateInSeconds The time in seconds before the rate limit resets for this API key.
+     * @param requestLimit       The maximum number of requests allowed within the reset rate period.
+     * @return A new {@link ApiKey} instance representing the generated unique API key.
+     */
     public static ApiKey generateUniqueKey(long resetRateInSeconds, long requestLimit) {
         ApiKeyDao dao = new ApiKeyDaoAdapter();
         Collection<String> apiKeysOnDb = dao.readAll().getResult().stream().map(ApiKey::getApiKey).toList();
         String randomKey = getRandomKey();
-        while(apiKeysOnDb.contains(randomKey)) {
+        while (apiKeysOnDb.contains(randomKey)) {
             randomKey = getRandomKey();
         }
         return new ApiKey(randomKey, resetRateInSeconds, requestLimit);
